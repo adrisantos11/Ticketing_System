9d8f977c9aaea6234ea88e32cc5d9d1c671a4a18
Adrián Santos Mena <adrisantos11.com>
2020-06-16 19:56:34 +0200

    Mejoras en los seeders

--- database/factories/IncidenciaFactory.php
+++ database/factories/IncidenciaFactory.php
@@ -6,21 +6,30 @@ use App\Incidencia;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
+
 $factory->define(Incidencia::class, function (Faker $faker) {
-    $randomDate1 = mt_rand(1262055681,1263645342);
+    $start = strtotime("3 March 2020");
+
+    $randomDate1 = mt_rand($start,time());
     $randomDateCasted1 = date("Y-m-d H:i:s", $randomDate1);
 
-    $randomDate2 = mt_rand(1262055681,1263645342);
+    $randomDate2 = mt_rand($randomDate1,time());
     $randomDateCasted2 = date("Y-m-d H:i:s", $randomDate2);
 
-    $ids_reporter = array(2,3,4,5);
+    $ids_reporter = array(2,3,4,5,6,7,8);
     $id_reporter = array_rand($ids_reporter, 1);
+    $supervisor = null;
+    
+    if ($ids_reporter[$id_reporter] == 2 || $ids_reporter[$id_reporter] == 3) {
+        $supervisor = $ids_reporter[$id_reporter];
+    }
 
-    $ids_assigned = array(4,5,6, null);
+    $ids_assigned = array(3,4,5,6, null);
     $id_assigned = array_rand($ids_assigned, 1);
 
-    $ids_supervisor = array(2,3, null);
-    $id_supervisor = array_rand($ids_supervisor, 1);
+    while ($ids_reporter[$id_reporter] == $ids_assigned[$id_assigned]) {
+        $id_assigned = array_rand($ids_assigned, 1);
+    }
 
     $categories = array("Mobiliario", "Wi-Fi", "Red", "Switch", "Hardware", "Software");
     $category = array_rand($categories, 1);
@@ -36,7 +45,7 @@ $factory->define(Incidencia::class, function (Faker $faker) {
         "id_reporter" => $ids_reporter[$id_reporter],
         "id_assigned" => $ids_assigned[$id_assigned],
         "id_team" => null,
-        "supervisor"=>$ids_supervisor[$id_supervisor],
+        "supervisor"=> $supervisor,
         "title" => Str::random(10),
         "description" => Str::random(30),
         "category" => $categories[$category],
--- database/seeds/UsuariosTableSeeder.php
+++ database/seeds/UsuariosTableSeeder.php
@@ -13,25 +13,25 @@ class UsuariosTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name'      => 'Maria Belén',
+            'name'      => 'Javier',
+            'surname1'  => 'Cai',
+            'surname2'  => 'Lin',
+            'exp'       => '00000000',
+            'password'  => Hash::make('123456'),
+            'email'     => 'javiercailin@gmail.com',
+            'phone'     => '000000000',
+            'role'      => 'admin',
+            'image_url' => '/images/default-profile-image.jpg'
+        ]);
+
+        DB::table('users')->insert([
+            'name'      => 'Mª Belén',
             'surname1'  => 'Mena',
             'surname2'  => 'Román',
             'exp'       => '00000001',
             'password'  => Hash::make('123456'),
             'email'     => 'mbmena1966@gmail.com',
             'phone'     => '660200506',
-            'role'      => 'visitor',
-            'image_url' => '/images/mbelen-mena.jpg'
-        ]);
-
-        DB::table('users')->insert([
-            'name'      => 'Javier',
-            'surname1'  => 'Cai',
-            'surname2'  => 'Lin',
-            'exp'       => '00000002',
-            'password'  => Hash::make('123456'),
-            'email'     => 'jcl4332@gmail.com',
-            'phone'     => '654345645',
             'role'      => 'supervisor'
         ]);
 
@@ -51,7 +51,7 @@ class UsuariosTableSeeder extends Seeder
             'name'      => 'Emilio Manuel',
             'surname1'  => 'Santos',
             'surname2'  => 'Cañon',
-            'exp'       => '00000004',
+            'exp'       => '00000002',
             'password'  => Hash::make('123456'),
             'email'     => 'manolitogafotas1966@gmail.com',
             'phone'     => '123456789',
@@ -63,7 +63,7 @@ class UsuariosTableSeeder extends Seeder
             'name'      => 'Pablo',
             'surname1'  => 'Elias',
             'surname2'  => 'Caballero',
-            'exp'       => '00000005',
+            'exp'       => '00000003',
             'password'  => Hash::make('123456'),
             'email'     => 'pablitoelias.elias@gmail.com',
             'phone'     => '123456789',
@@ -75,12 +75,24 @@ class UsuariosTableSeeder extends Seeder
             'name'      => 'Rodrigo',
             'surname1'  => 'Yagüe',
             'surname2'  => 'Piñuelas',
-            'exp'       => '00000006',
+            'exp'       => '00000004',
             'password'  => Hash::make('123456'),
             'email'     => 'adrian.santos2mena@gmail.com',
             'phone'     => '123456789',
             'role'      => 'technical',
             'image_url' => '/images/rodrigo-yagüe.png'
+        ]);
+
+        DB::table('users')->insert([
+            'name'      => 'Xichen',
+            'surname1'  => 'Zhou',
+            'surname2'  => '--',
+            'exp'       => '00000005',
+            'password'  => Hash::make('123456'),
+            'email'     => 'xichenzhou@gmail.com',
+            'phone'     => '123456789',
+            'role'      => 'technical',
+            'image_url' => '/images/xichen-zhou.png'
         ]);
 
         DB::table('users')->insert([
@@ -91,7 +103,7 @@ class UsuariosTableSeeder extends Seeder
             'password'  => Hash::make('123456'),
             'email'     => 'ejemplo@gmail.com',
             'phone'     => '000000000',
-            'role'      => 'admin',
+            'role'      => 'visitor',
             'image_url' => '/images/default-profile-image.jpg'
         ]);
     }
